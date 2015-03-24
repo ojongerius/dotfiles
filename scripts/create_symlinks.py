4e5f3ff017292bad6509262fe08fa1090c2bfca6
Otto Jongerius <ojongerius@atlassian.com>
2015-03-25 09:01:50 +1100

    Wrap line, print as a function.

--- scripts/create_symlinks.py
+++ scripts/create_symlinks.py
@@ -8,12 +8,12 @@ import errno
 
 def __symlink(source, destination):
         os.symlink(source, destination)
-        print 'INFO: Linked %s to %s' % (source, destination)
+        print('INFO: Linked %s to %s' % (source, destination))
 
 
 def __backup(destination):
         shutil.move(destination, '%s.orig' % destination)
-        print 'INFO: Created backup %s.orig' % (destination)
+        print('INFO: Created backup %s.orig' % (destination))
 
 
 def __create_links(source, destination):
@@ -21,13 +21,14 @@ def __create_links(source, destination):
             __symlink(source, destination)
         except OSError as e:
             if e.errno == errno.EEXIST:
-                if os.path.islink(destination) and os.path.realpath(source) == os.path.realpath(destination):
-                    print 'INFO: NOOP: %s LGTM.' % destination
+                if os.path.islink(destination) and \
+                        os.path.realpath(source) == os.path.realpath(destination):
+                    print('INFO: Nothing to do here: %s LGTM.' % destination)
                     return
                 __backup(destination)
                 __symlink(source, destination)
             else:
-                print 'ERROR: failed symlinking: %s' % e
+                print('ERROR: failed symlinking: %s' % e)
 
 
 def proccess_dotfiles(dir):
